193944948ac6fd9f222f09df77bf9e39ae37a495
Athe-Nah <louisa.belaidi@gmail.com>
2022-07-20 07:59:06 +0200

    ajout service et env var

---
+++ src/app/social-media/services/post.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import {HttpClient} from '@angular/common/http'
+import { Observable } from "rxjs";
+import { Post } from "../models/post.model";
+import { environment } from "src/environments/environment";
+
+@Injectable()
+
+export class PostService {
+
+  constructor(private http: HttpClient){}
+
+  getPost(): Observable<Post[]> {
+    return this.http.get<Post[]>(`${environment.apiUrl}/posts`);
+  }
+
+}
