9aeec7301412d2a34419b3814f29541f16262649
Athe-Nah <louisa.belaidi@gmail.com>
2022-07-20 07:58:17 +0200

    creation service  + ajout variable env pour getPost

---
+++ src/app/social-media/models/post.model.ts
@@ -0,0 +1,13 @@
+import { Comment } from "src/app/core/models/comment.model";
+
+
+export class Post {
+
+  id!: number;
+  userId!: number;
+  title!: string;
+  createdDate!: string;
+  content!: string;
+  comments!: Comment[];
+
+}
