23d2eb91c1cb10c6e7a36f2591c1a33e83d5cdbd
Athe-Nah <louisa.belaidi@gmail.com>
2022-07-20 07:10:35 +0200

    configuration du routage part 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // loas children pour le lazy loading
+  {path: "social-media", loadChildren:() => import ('./social-media/social-media.module').then(m =>m.SocialMediaModule)},
+  // Toutes les routes inconnues redirige vers social média pour l'instant
+  {path: '**', redirectTo:'social-media'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
